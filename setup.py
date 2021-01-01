67e317f3ce5331dc184c2fea2b211dbb8409503a
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-01-01 22:30:59 +0900

    version 0.6.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ URL="https://github.com/KoichiYasuoka/spaCy-ChaPAS"
 
 setuptools.setup(
   name="spacy_chapas",
-  version="0.6.6",
+  version="0.6.7",
   description="ChaPAS-CaboCha-MeCab wrapper for spaCy",
   long_description=long_description,
   long_description_content_type="text/markdown",
