b91c66d67b84cff17903755c6cc3e6c73424a728
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2022-03-14 16:31:03 +0900

    version 0.8.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ URL="https://github.com/KoichiYasuoka/spaCy-ChaPAS"
 
 setuptools.setup(
   name="spacy_chapas",
-  version="0.8.5",
+  version="0.8.6",
   description="ChaPAS-CaboCha-MeCab wrapper for spaCy",
   long_description=long_description,
   long_description_content_type="text/markdown",
@@ -16,7 +16,7 @@ setuptools.setup(
   license="MIT",
   keywords="spacy nlp",
   packages=setuptools.find_packages(),
-  install_requires=["spacy>=2.2.2","deplacy>=2.0.1"],
+  install_requires=["spacy>=2.2.2","deplacy>=2.0.2"],
   python_requires=">=3.6",
   package_data={"spacy_chapas":["./chapas2ud","./unidic2ipadic"]},
   data_files=[("bin",["spacy_chapas/chapas2ud"])],
